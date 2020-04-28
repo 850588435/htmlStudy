97d6b656b43054b2c57cd5e39aa4e118d61dc57a
850588435 <850588435@qq.com>
2020-04-28 12:33:29 +0800

    改进正则表达式

--- Animate_Ajax/animateLibrary.js
+++ Animate_Ajax/animateLibrary.js
@@ -2,11 +2,9 @@
 
 
     var cssCon="";
-    var regex=/\.[a-zA-Z0-9_\-]*{/g;
     var arr=new Array();
     var fei_arr=12;
     var str="";
-    var num=78;
     var myCollection=new Array();
 
     function color16(){//rgb颜色随机
@@ -26,33 +24,26 @@
             if (xmlhttp.readyState==4 && xmlhttp.status==200)
             {
                 cssCon=xmlhttp.responseText;
-                arr=cssCon.match(regex);
+                arr=cssCon.match(/@keyframes[ a-zA-Z0-9_\-]*{/g);
                 for(var i=0;i<arr.length;i++){
-                    arr[i]=arr[i].slice(1,-1);
-
-                }
-                for(var j=0;j<fei_arr;j++){
-                    //删除数组最后12个class
-                    arr.splice((arr.length-1),1);
-                    
+                    arr[i]=arr[i].slice(11,-1);
                 }
 
-                str+="<p>Animate.css样式预览</p><div class='con'>";
-                for(var i=0;i<num;i++){
-                    str+="<div class='aaa'><p id='"+arr[i]+"' class='"+arr[i]+" animated'>Animate.css</p><span>"+arr[i]+"</span></div>";
+                str+="<header>Animate.css样式预览</header><div class='con'>";
+                for(var i=0;i<arr.length;i++){
+                    str+="<div class='card'><p id='"+arr[i]+"' class='"+arr[i]+" animated'>Animate.css</p><span>"+arr[i]+"</span></div>";
                 }
                 str+="</div>";              
                 document.getElementsByTagName("body")[0].innerHTML=str;
-                for(var i=0;i<num;i++){
+                for(var i=0;i<arr.length;i++){
                     document.getElementById(arr[i]).style.color=color16();
                 }
+
             }
         }
         xmlhttp.open("GET","https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css",true);
         xmlhttp.send();
     }
-
-    
 
     window.onload=function(){
         getAnimate();
